c3acfad6ac12d97d2e8d921accb41a5e7dca120a
Michael Eaton <michael.eaton@edu.tbz.ch>
2026-02-02 21:12:09 +0100

    favicon

--- imprint/script.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  // Optional: allow embedding the page as a clean section
-  try {
-    const qs = new URLSearchParams(location.search);
-    if (qs.get('embed') === '1') document.body.classList.add('is-embed');
-  } catch (e) {}
-})();
